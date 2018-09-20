dc859677904101e9ff4ea4728509ff555cf8ade6
BobGeews <ossi.hakkinen@hotmail.fi>
2018-09-20 09:02:07 +0300

    Iftask2 1.0

--- condition-tasks/Iftask2/Iftask2/Program.cs
+++ condition-tasks/Iftask2/Iftask2/Program.cs
@@ -6,7 +6,19 @@ namespace Iftask2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Tämä ohjelma selvittää onko luku parillinen vai pariton.");
+            Console.Write("Syötä numero. ");
+            string userInput = Console.ReadLine();
+            int number = int.Parse(userInput);
+
+            if (number % 2 == 0)
+                Console.WriteLine($"Luku {number} on parillinen.");
+            else
+                Console.WriteLine($"Luku {number} on pariton.");
+
+
+
+        
         }
     }
 }
