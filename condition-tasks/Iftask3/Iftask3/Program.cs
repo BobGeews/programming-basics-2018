e99170bf8c2966b55376931ab2d131adeb51e132
BobGeews <ossi.hakkinen@hotmail.fi>
2018-09-20 09:43:32 +0300

    Iftask3 started

---
+++ condition-tasks/Iftask3/Iftask3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Iftask3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Onko numero parillinen vai pariton vai negatiivinen tai positiivinen.");
+            Console.Write("Syötä numero. ");
+            string userInput = Console.ReadLine();
+
+        }
+    }
+}
