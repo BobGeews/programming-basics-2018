b9c7d1f8b88950710fabc4eacee3f6f78e116c9f
BobGeews <ossi.hakkinen@hotmail.fi>
2018-10-01 12:21:35 +0300

    Ticket-price-started

---
+++ condition-tasks/ticketprice/ticketprice/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ticketprice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+                Console.WriteLine("Ohjelma selvittää maatalousnäyttelyn lipunhinnan.");
+                int ticketPrice = 16;
+                double discount = 0.15; //15% ale
+                
+
+                Console.Write("Oletko varusmies? ");
+            string userInput = Console.ReadLine();
+            
+
+
+
+          
+
+
+
+
+
+
+                Console.WriteLine($"Lipunhintasi on {ticketPrice - ticketPrice * discount} €");
+
+
+            }
+        }
+    }
+
+    
+
