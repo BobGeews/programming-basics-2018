cd61c1418cd5e4bab720c03b4df68a070a46358b
BobGeews <ossi.hakkinen@hotmail.fi>
2018-10-02 20:20:20 +0300

    little bit progress

--- condition-tasks/ticketprice/ticketprice/Program.cs
+++ condition-tasks/ticketprice/ticketprice/Program.cs
@@ -6,27 +6,44 @@ namespace ticketprice
     {
         static void Main(string[] args)
         {
+
+
+            Console.WriteLine("Ohjelma selvittää maatalousnäyttelyn lipunhinnan.");
+            int ticketPrice = 16;
+            double discount = 0.00; //ale
+            int age = 0;
+            string userInput;
+            
+                Console.Write("Oletko varusmies k/e ");
+                userInput = Console.ReadLine();
+
+                if
+
+                     (userInput == "k")
+                    discount = 0.50;
+
+                else
+
+                    Console.Write("Oletko opiskelija k/e ");
+                userInput = Console.ReadLine();
             
 
-                Console.WriteLine("Ohjelma selvittää maatalousnäyttelyn lipunhinnan.");
-                int ticketPrice = 16;
-                double discount = 0.15; //15% ale
-                
+                if
 
-                Console.Write("Oletko varusmies? ");
-            string userInput = Console.ReadLine();
+                (userInput == "k")
+
+                    Console.Write("Oletko MTK jäsen k/e ");
+                userInput = Console.ReadLine();
+
+                if
+
+                (userInput == "k")
+                    discount = 0.60;
+
+                else
+                    discount = 0.45;
             
-
-
-
-          
-
-
-
-
-
-
-                Console.WriteLine($"Lipunhintasi on {ticketPrice - ticketPrice * discount} €");
+           Console.WriteLine($"Lipunhintasi on {ticketPrice - ticketPrice * discount} €");
 
 
             }
