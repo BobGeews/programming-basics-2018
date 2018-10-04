756f94ed9181f9ed53dfb65eb547311bdbdb3305
BobGeews <ossi.hakkinen@hotmail.fi>
2018-10-04 10:54:00 +0300

    copied loop task 3.2

---
+++ loop-tasks/Looptask3_4/Looptask3_4/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Looptask3_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tämä ohjelma laskee annetun luvun summan.");
+            int number = 0;
+            int i = 1;
+            int summ = 0;
+            do
+            {
+                Console.Write("Syötä luku: ");
+                number = int.Parse(Console.ReadLine());
+
+                
+                
+
+            } while (number <= 0);
+
+            while (i <= number)
+            {
+                summ = summ + i; //summ=+i
+                i = i + 1; //i++;
+            }
+            Console.WriteLine($"Luvun {number}! = {summ}");
+        }
+    }
+}
