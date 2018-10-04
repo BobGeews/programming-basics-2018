c0082d6ef92113f69c57f6a9179cb911e45ae129
BobGeews <ossi.hakkinen@hotmail.fi>
2018-10-04 18:06:18 +0300

    Finished!

--- loop-tasks/Looptask3_4/Looptask3_4/Program.cs
+++ loop-tasks/Looptask3_4/Looptask3_4/Program.cs
@@ -8,22 +8,32 @@ namespace Looptask3_4
         {
             Console.WriteLine("Tämä ohjelma laskee annetun luvun summan.");
             int number = 0;
-            int i = 1;
+            int i = 0;
             int summ = 0;
             do
             {
                 Console.Write("Syötä luku: ");
                 number = int.Parse(Console.ReadLine());
 
-                
-                
+                if (number == 0)
+                {
+                    Console.WriteLine("Virheellinen syöte!");
+                }
 
-            } while (number <= 0);
+            } while (number == 0);
 
-            while (i <= number)
+            if (number > 0)
+                while (i <= number)
+
+                {
+                    summ = summ + i; //summ=+i
+                    i = i + 1; //i++;
+                }
+            else
+            while (i >= number)
             {
-                summ = summ + i; //summ=+i
-                i = i + 1; //i++;
+                summ = summ + i;
+                i = i - 1;
             }
             Console.WriteLine($"Luvun {number}! = {summ}");
         }
