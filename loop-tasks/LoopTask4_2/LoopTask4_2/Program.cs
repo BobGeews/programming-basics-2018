f7916181fc36a5e2caac748446e4285cf5c8b1d1
BobGeews <ossi.hakkinen@hotmail.fi>
2018-10-10 09:51:16 +0300

    Loop task 4.2 completed

---
+++ loop-tasks/LoopTask4_2/LoopTask4_2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace LoopTask4_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+                     
+            Console.WriteLine("Tämä ohjelma näyttää kertotaulun luvuille 1-9.");
+            Console.Write("Syötä luku: ");
+            number = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i < 10; i++)
+            {
+                Console.WriteLine($"{number} x {i} = {number*i}");
+            }
+        }
+    }
+}
