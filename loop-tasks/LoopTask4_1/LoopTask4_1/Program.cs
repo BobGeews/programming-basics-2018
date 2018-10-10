812218a3b61078cf9bfe31b20678d97deb649bf9
BobGeews <ossi.hakkinen@hotmail.fi>
2018-10-10 09:21:33 +0300

    Loop Task 4.1 completed

---
+++ loop-tasks/LoopTask4_1/LoopTask4_1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace LoopTask4_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Tämä ohjelma tulostaa kokonaisluvut 1-10 ja niiden neliöjuuret.");
+            for (int i = 1; i <= 10; i++)
+            {                
+                Console.WriteLine($"{i}\t {Math.Sqrt(i)}");
+            } 
+        }
+
+    }
+}   
