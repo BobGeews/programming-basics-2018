2163da2bcb70fc7f9d6f4c15ae8a416fe6a026b7
BobGeews <ossi.hakkinen@hotmail.fi>
2018-10-11 09:57:00 +0300

    loop task 4.4 started

---
+++ loop-tasks/Looptask4_4/Looptask4_4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Looptask4_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kolikonheitto");
+            Console.Write("Kuinka monta kertaa heitetään kolikkoa?: ");
+            int number;
+            int klaava = 0;
+            int kruuna = 1;
+
+            number = int.Parse(Console.ReadLine());
+            Random rnd = new Random();
+
+            
+        }
+    }
+}
