4780ff04d9d54dcf79e065bf74530db892dee88e
BobGeews <ossi.hakkinen@hotmail.fi>
2018-10-11 13:14:21 +0300

    looptask4.4 finished

--- loop-tasks/Looptask4_4/Looptask4_4/Program.cs
+++ loop-tasks/Looptask4_4/Looptask4_4/Program.cs
@@ -7,15 +7,42 @@ namespace Looptask4_4
         static void Main(string[] args)
         {
             Console.WriteLine("Kolikonheitto");
-            Console.Write("Kuinka monta kertaa heitetään kolikkoa?: ");
             int number;
             int klaava = 0;
-            int kruuna = 1;
-
-            number = int.Parse(Console.ReadLine());
+            int kruuna = 0;
+            int rndNumber;
             Random rnd = new Random();
+            
+                Console.Write("Kuinka monta kertaa heitetään kolikkoa?: ");
+                number = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < number; i++)
+            {
+                rndNumber = rnd.Next(2);
+                if (rndNumber == 1)
+                {
+                    kruuna = kruuna + 1;
+                }
+                else
+                {
+                    klaava = klaava + 1;
+                }
+            }
+
+                Console.WriteLine($"Heittojen määrä {number}");
+                Console.WriteLine($"Kruunien määrä {kruuna}");
+                Console.WriteLine($"Klaavojen määrä {klaava}");
+                    
 
             
+            
+
+
+
+
+
+
+
         }
     }
 }
