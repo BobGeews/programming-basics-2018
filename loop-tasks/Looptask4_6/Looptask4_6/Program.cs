68baf7d7e53ce9f820b1225ca836fd443ccd7ba2
BobGeews <ossi.hakkinen@hotmail.fi>
2018-10-16 15:34:41 +0300

    looptask 4.6 started

---
+++ loop-tasks/Looptask4_6/Looptask4_6/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Looptask4_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tämä ohjelma simuloi nopanheittoa 1000 kertaa ja näyttää kuinka monta kuutosta arvottiin.");
+            Random rnd = new Random();
+            int randomRnd;
+            
+
+            for (int i = 1; i < 1001; i++)
+            {
+                randomRnd = rnd.Next(7);
+                Console.WriteLine($"{i}\t {randomRnd}");
+
+            }
+            
+            
+
+            
+        }
+    }
+}
