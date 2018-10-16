b50defe15dae32d2421413af20113c4e51933613
BobGeews <ossi.hakkinen@hotmail.fi>
2018-10-16 14:44:34 +0300

    Loop Task 4.5 ready

--- loop-tasks/Looptask4_5/Looptask4_5/Program.cs
+++ loop-tasks/Looptask4_5/Looptask4_5/Program.cs
@@ -9,11 +9,23 @@ namespace Looptask4_5
             Console.WriteLine("Tämä ohjelma tulostaa vakioveikkauksen rivin jossa 13 kohdetta. 1=kotivoitto, x=tasapeli, 2=vierasvoitto.");
             double rndNumber;
             Random rnd = new Random();
-            for (int i = 1; i < 13; i++)
+            for (int i = 1; i < 14; i++)
             {
               rndNumber = rnd.NextDouble();
                 if (rndNumber <= 0.4)
-                    Console.Writeline($"{i}/t x");
+                {
+                    Console.WriteLine($"{i}\t 1");
+
+                }
+                else if (rndNumber > 0.4 && rndNumber < 0.7)
+                {
+                    Console.WriteLine($"{i}\t x");
+                }
+                else
+                {
+                    Console.WriteLine($"{i}\t 2");
+                }
+
 
 
 
