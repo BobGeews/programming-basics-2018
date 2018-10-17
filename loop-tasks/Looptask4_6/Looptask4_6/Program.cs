82dd03209dd42ac65f988acdc359c9b84cb89a95
BobGeews <ossi.hakkinen@hotmail.fi>
2018-10-17 13:45:29 +0300

    Loop task 4.6 ready

--- loop-tasks/Looptask4_6/Looptask4_6/Program.cs
+++ loop-tasks/Looptask4_6/Looptask4_6/Program.cs
@@ -9,15 +9,26 @@ namespace Looptask4_6
             Console.WriteLine("Tämä ohjelma simuloi nopanheittoa 1000 kertaa ja näyttää kuinka monta kuutosta arvottiin.");
             Random rnd = new Random();
             int randomRnd;
+            int z = 0;
             
-
             for (int i = 1; i < 1001; i++)
             {
-                randomRnd = rnd.Next(7);
+                
+                do
+                {
+                    randomRnd = rnd.Next(7);
+
+                } while (randomRnd == 0);
+
+                if (randomRnd == 6)
+                {
+                    z++;
+                }
+
                 Console.WriteLine($"{i}\t {randomRnd}");
 
             }
-            
+            Console.WriteLine($"Kuutonen esiintyi {z} kertaa.");
             
 
             
