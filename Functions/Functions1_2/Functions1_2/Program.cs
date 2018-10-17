f69eb42e9f295d9e7a1e52c49943fc3ea10db432
BobGeews <ossi.hakkinen@hotmail.fi>
2018-10-17 15:21:07 +0300

    Function 1.2 ready

---
+++ Functions/Functions1_2/Functions1_2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Functions1_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y;
+            Intro(out x, out y);
+            Console.WriteLine(SmallerNumber(x, y));
+        }
+        static void Intro(out int number1, out int number2)
+        {
+            Console.WriteLine("Ohjelma palauttaa kahdesta annetusta luvasta pienemmän");
+            Console.Write("Syötä 1. numero. ");
+            number1 = int.Parse(Console.ReadLine());
+            Console.Write("Syötä 2. numero. ");
+            number2 = int.Parse(Console.ReadLine());
+        }
+
+        static string SmallerNumber(int a, int b)
+        {
+            if (a == b)
+                return $"Luku on {a} on yhtäsuuri kuin {b}";
+            else if (a < b) 
+                return $"Luku {a} on pienempi kuin {b}";
+            else
+                return $"Luku {b} on pienempi kuin {a}";
+        } 
+    }
+}
