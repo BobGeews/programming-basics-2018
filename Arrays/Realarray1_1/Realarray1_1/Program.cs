aa8a8ea4104a72394e36164740e007286d0b609f
BobGeews <ossi.hakkinen@hotmail.fi>
2018-10-31 13:30:38 +0200

    Array1.1 ready

---
+++ Arrays/Realarray1_1/Realarray1_1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Realarray1_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tuotteiden hinta taulukko.");
+            decimal[] productprice = new decimal[] { 7.96m, 72.0m, 99.90m };
+            int[] productamount = new int[] { 1, 3, 2 };
+            decimal[] producttotalprice = new decimal[3];
+
+            for (int i = 0; i < producttotalprice.Length; i++)
+            {
+                producttotalprice[i] = productamount[i] * productprice[i];
+                Console.WriteLine($"Tuote {i + 1}. {producttotalprice[i]:c}");
+            }
+            
+        }
+    }
+}
