5fd0b6dac910dfba99a9fa546698b019ba878d8c
BobGeews <ossi.hakkinen@hotmail.fi>
2018-11-12 11:03:51 +0200

    Merkijono task 1 completed

---
+++ Arrays/merkkijono1/merkkijono1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace merkkijono1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Kirjoita jotain: ");
+            string userInput = Console.ReadLine();
+            Console.WriteLine($"Syöttessä on {userInput.Length} kirjainta.");
+
+            
+        }
+    }
+}
