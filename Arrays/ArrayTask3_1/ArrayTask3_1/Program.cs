0597e5333dcd75e4187d31aca78ab46eba14b011
BobGeews <ossi.hakkinen@hotmail.fi>
2018-11-14 19:05:02 +0200

    started

---
+++ Arrays/ArrayTask3_1/ArrayTask3_1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ArrayTask3_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma joka arpoo taulukkoon lukuja 0-20 väliltä.");
+            int[] taulukko = new int[10];
+            Random rnd = new Random();
+
+            for (int i = 0; i < taulukko.Length; i++)
+            {
+                taulukko[i] = rnd.Next(20);              
+            }
+        }
+    }
+}
