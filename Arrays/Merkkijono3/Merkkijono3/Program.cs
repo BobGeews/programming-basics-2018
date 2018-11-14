87973731acc1c0b3e4dd7c147ae02127678a3607
BobGeews <ossi.hakkinen@hotmail.fi>
2018-11-14 19:06:26 +0200

    started, forget to commit and push

---
+++ Arrays/Merkkijono3/Merkkijono3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Merkkijono3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tämä ohjelma laskee k kirjainten määrän syötteessä.");
+            Console.Write("Kirjoita jotain: ");
+            string userInput = Console.ReadLine();
+
+            for (int i = 0; i < userInput.Length; i++)
+            {
+                
+            }
+            
+        }
+    }
+}
