a3def8a0f49306a8ccabfbfcdd6680185cc0b589
BobGeews <ossi.hakkinen@hotmail.fi>
2018-12-03 09:56:00 +0200

    Reference number project initialized and userinterface created. Still much work to do.

---
+++ File-handling/reference-number/reference-number/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+
+
+namespace reference_number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tämä ohjelma joko tarkistaa viitenumeron, luo uuden viitenumeron tai luo halutun määrän viitenumeroita.");
+
+            string choise = null;
+
+            do
+            {
+                choise = userInterface();
+
+                switch (choise.ToUpper())
+                {
+                    case "Q":
+                        {
+                            
+
+
+
+                            break;
+                        }
+                    case "W":
+                    break;
+
+                    case "E":
+                    break;
+                }
+            } while (choise.ToUpper() != "X");
+
+            string userInterface()
+            {
+                Console.WriteLine("[Q] Tarkasta jo olemassa oleva viitenumero.");
+                Console.WriteLine("[W] Generoi viitenumero.");
+                Console.WriteLine("[E] Generoi haluttu määrä viitenumeroita.");
+                Console.WriteLine("[X] Sulje ohjelma.");
+                return Console.ReadLine();
+            }
+
+            
+            
+                
+            
+        }
+    }
+}
