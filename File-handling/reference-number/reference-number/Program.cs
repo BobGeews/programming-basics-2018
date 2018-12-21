bd75adc6d22aa7a382bc519be01aa4e6bf0892a0
BobGeews <ossi.hakkinen@hotmail.fi>
2018-12-21 16:37:41 +0200

    Referencenumber checker working.

--- File-handling/reference-number/reference-number/Program.cs
+++ File-handling/reference-number/reference-number/Program.cs
@@ -6,58 +6,111 @@ namespace reference_number
 {
     class Program
     {
+
         static void Main(string[] args)
         {
             Console.WriteLine("Tämä ohjelma joko tarkistaa viitenumeron, luo uuden viitenumeron tai luo halutun määrän viitenumeroita.");
+
 
             string choise = null;
 
             do
             {
-                choise = userInterface();
-
+                choise = UserInterface();
                 switch (choise.ToUpper())
+
                 {
                     case "Q":
+
+                        if (Viitteentarkistus())
                         {
-                            //////WARNING//////SHIT CODE BELOW
-                            string multipliers = "7, 3, 1";
-                            Console.Write("Syötä viitenumero ");
-                            string userInput = Console.ReadLine();
+                            Console.WriteLine("Syöttämäsi viitenumero on oikein");
 
-                            int[] referenceNumber = new int[userInput.Length];
-                            char lastNumber = userInput[userInput.Length - 1];
-                            userInput = userInput.Remove(userInput.Length, 1);
-
-                            
-
-
-
-
-
-                            break;
                         }
+                        else
+                        {
+                            Console.WriteLine("Syöttämäsi viitenumero on väärin");
+                        }
+                        break;
+
                     case "W":
-                    break;
+
+                        
+
+
+                        break;
 
                     case "E":
-                    break;
-                }
-            } while (choise.ToUpper() != "X");
 
-            string userInterface()
+
+                        break;
+
+                    default:
+                        Console.WriteLine("Valitse Q, W, E, X");
+                        break;
+
+                }
+            }
+            while (choise.ToUpper() != "X");
+
+
+        }
+
+        static string UserInterface()
+        {
+            Console.WriteLine();
+            Console.WriteLine("[Q] Tarkasta jo olemassa oleva viitenumero.");
+            Console.WriteLine("[W] Generoi viitenumeron.");
+            Console.WriteLine("[E] Generoi haluttu määrä viitenumeroita.");
+            Console.WriteLine("[X] Sulje ohjelma.");
+
+            return Console.ReadLine();
+        }
+
+
+        static bool Viitteentarkistus()
+        {
+            Console.Write("Syötä viitenumero: ");
+            string refNum = Console.ReadLine();
+
+            string kertoimet = "731";
+            int bump = 0;
+
+            for (int i = 0; i < refNum.Length - 1; i++)
             {
-                Console.WriteLine("[Q] Tarkasta jo olemassa oleva viitenumero.");
-                Console.WriteLine("[W] Generoi viitenumero.");
-                Console.WriteLine("[E] Generoi haluttu määrä viitenumeroita.");
-                Console.WriteLine("[X] Sulje ohjelma.");
-                return Console.ReadLine();
+                bump += int.Parse(refNum[refNum.Length - 2 - i].ToString()) * int.Parse(kertoimet[i % 3].ToString());
+
+
+            }
+            
+
+            int seuraavakybä = (int)Math.Ceiling((decimal)bump / 10) * 10;
+            int tarkiste = seuraavakybä - bump;
+
+            if (tarkiste == 0)
+            {
+                tarkiste = 0;
+            }
+            if (refNum[refNum.Length - 1].ToString() == tarkiste.ToString())
+            {
+                return true;
+            }
+            else
+            {
+                return false;
             }
 
-            
-            
-                
-            
         }
+
+
+
+
+        
+        
+
+
+
+
     }
+
 }
