9c5e9c13d80b17dd35494d731cb30213d30b1dc5
BobGeews <ossi.hakkinen@hotmail.fi>
2019-10-02 08:31:27 +0300

    Forgot to push all these files.

---
+++ Arrays/vokaalilaskuri/vokaalilaskuri/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace vokaalilaskuri
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma laskee kuinka monta vokaalia on käyttäjän syötteessä.");
+            string userInput = UserInput();
+            string removedUserInput = VowelsRemove(userInput);
+            Console.WriteLine($"Vokaaleita sanassa {userInput} on {userInput.Length - removedUserInput.Length}");
+        }
+
+        static string UserInput()
+        {
+            Console.Write("kirjoita jotain: ");
+            return Console.ReadLine().ToUpper();
+        }
+        static string VowelsRemove(string d)
+        {
+            for (int i = 0; i < d.Length; i++)
+            {
+                if (d[i] == 'A' || d[i] == 'E' || d[i] == 'I' || d[i] == 'O' || d[i] == 'U' || d[i] == 'Y' || d[i] == 'Ä' || d[i] == 'Ö')
+                    // d = d.Replace(d[i].ToString(), "");
+                    d = d.Remove(i, 1);
+                i--;
+                   
+            }
+            return d;
+            
+        }
+       
+    }
+    
+}
--- loop-tasks/loopTask3_1/loopTask3_1/Program.cs
+++ loop-tasks/loopTask3_1/loopTask3_1/Program.cs
@@ -1,36 +1,150 @@
 ﻿using System;
 
-namespace loopTask3_1
+using System.IO;
+
+namespace ReferenceNumber
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Tämä ohjelma laskee annetun luvun summan.");
-            int number = 0;
-            int i = 1;
-            int fact = 1;
+            string choice = null;
+            //string path = @"C:\TEMP\referencenumber.txt";
+            string message = "";
+
             do
             {
-                Console.Write("Syötä luku: ");
-                number = int.Parse(Console.ReadLine());
-
-                if (number <= 0)
+                choice = UserInterface();
+                switch (choice.ToUpper())
                 {
-                    Console.WriteLine("Virheellinen syöte!");
+                    case "1":
+                        if (CheckRefNum())
+                        {
+                            message = "\nSyöttämäsi viitenumero on oikein.";
+                        }
+                        else
+                        {
+                            message = "\nSyöttämäsi viitenumero on väärin.";
+                        }
+                        break;
+
+                    case "2":
+                        
+                        
+                        message = "\nKotimainen viitenumero on luotu.";
+                        break;
+
+                    case "3":
+                        //CreateRefNumberFile(path);
+                        message = "\nHaluamasi määrä viitenumeroita on luotu ja tallennettu tiedostoon.";
+                        break;
+
+                    case "X":
+                        message = "\nOhjelman suoritus on päättynyt. Paina enter.";
+                        break;
+
+                    default:
+                        message = "Komentoa ei löydy. Valitse 1, 2, 3 tai X poistuaksesi ohjelmasta";
+                        break;
                 }
 
-            } while (number <= 0);
-
-            while (i <=number)
-            {
-                fact = fact * i; //fact=*i
-                i = i + 1; //i++;
+                Console.WriteLine(message);
+                Console.ReadLine();
+                Console.Clear();
             }
-            Console.WriteLine($"Luvun {number}! = {fact}");
+
+            while (choice.ToUpper() != "X");
+
         }
 
+        static string UserInterface()
+        {
+            Console.WriteLine("Tervetuloa viitenumero-ohjelmaan! Syötä haluamasi vaihtoehto.");
+            Console.WriteLine("( 1 ) Tarkasta kotimainen viitenumero");
+            Console.WriteLine("( 2 ) Luo kotimainen viitenumero");
+            Console.WriteLine("( 3 ) Luo kotimaisia viitenumeroja ja tallenna ne tiedostoon");
+            Console.WriteLine("( X ) Lopeta ohjelmansuoritus");
+
+            Console.WriteLine();
+
+            Console.Write("Mitä tehdään? ");
+
+            return Console.ReadLine();
+        }
+
+        static bool CheckRefNum()
+        {
+            Console.WriteLine("Syötä kotimainen viitenumero.");
+            string refNumber = Console.ReadLine();
+
+            string multiplier = "731";
+            int addUp = 0;
+            int checkerNumber;
+            int nextTenth;
+
+            for (int i = 0; i < refNumber.Length - 1; i++)
+            {
+                addUp += int.Parse(refNumber[refNumber.Length - 2 - i].ToString()) * int.Parse(multiplier[i % 3].ToString());
+                ;
+            }
+
+            nextTenth = (int)Math.Ceiling((decimal)addUp / 10) * 10;
+            checkerNumber = nextTenth - addUp;
+
+            if (checkerNumber == 10)
+            {
+                checkerNumber = 0;
+            }
+
+            if (refNumber[refNumber.Length - 1].ToString() == checkerNumber.ToString())
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+        static void CreateRefNum(int length)
+        {
+            var numbers = "0123456789";
+            string multiplier = "731";
+            int addUp = 0;
+            int nextTenth;
+            int checkerNumber;
+
+            var refNumbers = new char[length];
+            Random aNumber = new Random();
+            for (int i = 0; i < length; i++)
+            {
+                refNumbers[i] = numbers[aNumber.Next(length)];
+                if (refNumbers[0] == '0')
+                {
+                    i--;
+                }
+            }
+
+            var finalRefNumbers = new String(refNumbers);
+
+            for (int i = 0; i < length; i++)
+            {
+                addUp += int.Parse(finalRefNumbers[length - 1 - i].ToString()) * int.Parse(multiplier[i % 3].ToString());
+                ;
+            }
+
+            nextTenth = (int)Math.Ceiling((decimal)addUp / 10) * 10;
+            checkerNumber = nextTenth - addUp;
+
+            if (checkerNumber == 10)
+            {
+                checkerNumber = 0;
+            }
+
+            Console.WriteLine($"Uusi viitenumero: {finalRefNumbers.Insert(3, " ")}{checkerNumber}");
+        }
     }
 }
-    
+
 
